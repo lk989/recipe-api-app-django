38b0573b6bc20cb06b6aca837992fff73fcadd71
Lolo <lma.bugis@hotmail.com>
2025-08-09 01:08:30 +0300

    Add wait_for_db command

--- app/core/management/commands/wait_for_db.py
+++ app/core/management/commands/wait_for_db.py
@@ -2,11 +2,29 @@
     Django command to wait for the database to be available.
 """
 
-form django.core.management.base import BaseCommand
+import time
+
+from psycopg2 import OperationalError as Psycopg2Error
+
+from django.db.utils import OperationalError
+from django.core.management.base import BaseCommand
+
 
 class Command(BaseCommand):
     """Django command to wait for the database to be available."""
 
     def handle(self, *args, **options):
         """Handle the command."""
-        pass
+        self.stdout.write("Waiting for database...")
+        db_up = False
+
+        while db_up is False:
+            try:
+                # Attempt to connect to the database
+                self.check(databases=['default'])
+                db_up = True
+            except (Psycopg2Error, OperationalError):
+                self.stdout.write("Database unavailable, waiting 1 second...")
+                time.sleep(1)
+
+        self.stdout.write(self.style.SUCCESS("Database available!"))
--- app/core/tests/test_commands.py
+++ app/core/tests/test_commands.py
@@ -10,9 +10,10 @@ from django.core.management import call_command
 from django.db.utils import OperationalError
 from django.test import SimpleTestCase
 
- # Mock the check method to simulate database availability
- # check is a method in the Command class that checks
-@patch('core.management.commands.wait_for_db.Command.chech')
+
+# Mock the check method to simulate database availability
+# check is a method in the Command class that checks
+@patch('core.management.commands.wait_for_db.Command.check')
 class CommandTests(SimpleTestCase):
     """Test commands."""
 
@@ -22,17 +23,19 @@ class CommandTests(SimpleTestCase):
 
         call_command('wait_for_db')
 
-        patched_checkc.assert_called_once_with(database=['default'])
+        patched_checkc.assert_called_once_with(databases=['default'])
 
+    # arguments should be put from the inside out.
+    # like here: sleep before check
     @patch('time.sleep')
-    # arguments should be put from the inside out. like here: sleep before check
     def test_for_db_delay(self, patched_sleep, patched_checkc):
         """Test waiting for database when database is not available."""
         # Simulate the database being unavailable for a few checks
         # side_effect allows us to specify a sequence of return values
-        patched_checkc.side_effect = [Psycopg2Error] * 2 + [OperationalError] * 3 + [True]
+        patched_checkc.side_effect = [Psycopg2Error] * 2 + \
+            [OperationalError] * 3 + [True]
 
         call_command('wait_for_db')
 
         self.assertEqual(patched_checkc.call_count, 6)
-        patched_checkc.assert_called_with(database=['default'])
+        patched_checkc.assert_called_with(databases=['default'])
