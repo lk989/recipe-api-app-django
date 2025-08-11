3dfa307d250b95bd4ffb6ad8bcd10cbb4e5e6290
Lolo <lma.bugis@hotmail.com>
2025-08-11 11:28:00 +0300

    List users in admin site

--- app/core/admin.py
+++ app/core/admin.py
@@ -1,3 +1,20 @@
-from django.contrib import admin  # noga: F401
+"""
+Django admin customization.
+"""
 
-# Register your models here.
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+
+from core import models
+
+
+class UserAdmin(BaseUserAdmin):
+    """Define the admin pages for users."""
+
+    ordering = ['id']
+    list_display = ['email', 'name']
+
+
+# Second argument is not required.
+# It is used to specify the model admin class.
+admin.site.register(models.User, UserAdmin)
