84bb54e47d41adde5a915c95cdfafc538baec831
Lolo <lma.bugis@hotmail.com>
2025-08-11 11:21:03 +0300

    write test case for listing users in admin site

---
+++ app/core/tests/test_admin.py
@@ -0,0 +1,34 @@
+"""
+Test for the Django admin modifications.
+"""
+
+from django.test import TestCase, Client
+from django.contrib.auth import get_user_model
+from django.urls import reverse
+
+
+class AdminSiteTests(TestCase):
+    """Test the Django admin modifications."""
+
+    def setUp(self):
+        """Create user and client for testing"""
+        self.client = Client()
+        self.admin_user = get_user_model().objects.create_superuser(
+            'admin@example.com',
+            'testpass123'
+        )
+
+        self.client.force_login(self.admin_user)
+        self.user = get_user_model().objects.create_user(
+            email='user@example.com',
+            password='testpass123',
+            name='Test User'
+        )
+
+    def test_users_listed(self):
+        """Test that users are listed on the user page."""
+        url = reverse('admin:core_user_changelist')
+        res = self.client.get(url)
+
+        self.assertContains(res, self.user.email)
+        self.assertContains(res, self.user.name)
